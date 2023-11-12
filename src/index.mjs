a9921202a61c5e1bc6d966d2f97b4aec4718d602
wobsoriano <sorianorobertc@gmail.com>
2023-11-12 10:12:13 -0800

    set max tokens

--- src/index.mjs
+++ src/index.mjs
@@ -81,7 +81,7 @@ bot.command('code', async (ctx) => {
 
   const completion = await openai.chat.completions.create({
     messages,
-    model: 'gpt-4'
+    model: 'gpt-4',
   })
   const completionAnswer = completion.choices[0].message.content
   messages.push({
@@ -113,7 +113,9 @@ bot.on(message('text'), async (ctx) => {
 
   const completion = await openai.chat.completions.create({
     messages,
-    model: 'gpt-3.5-turbo'
+    model: 'gpt-3.5-turbo',
+    temperature: 0.5,
+    max_tokens: 1024
   })
   const completionAnswer = completion.choices[0].message.content
   messages.push({
